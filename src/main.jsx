3a68f610b56acc4599fc4a78a7360786548ca145
Shumail2 <info.clothingmagnet@gmail.com>
2026-01-23 12:26:32 +0530

    Fix project structure and Vite build

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import RecipeProvider from "./context/RecipeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <RecipeProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <ToastContainer />
-    </BrowserRouter>
+    </HashRouter>
   </RecipeProvider>
 );
